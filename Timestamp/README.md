2105c4f4266005beffc4655203be22c1ce950e00
Tosmim <tosmimforidmehtab@gmail.com>
2023-09-21 11:09:59 +0530

    Timestamp Project done

---
+++ Timestamp/README.md
@@ -0,0 +1,3 @@
+# Timestamp Microservice
+
+This is the completed project
---
+++ Timestamp/index.js
@@ -0,0 +1,43 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+//Check valid date
+const isValid = (date) => {
+    const d = new Date(date);
+    const valid = d.toString() !== "Invalid Date";
+    return valid;
+};
+
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+    let date = new Date(req.params.date);
+    if (!isValid(date)) date = new Date(parseInt(req.params.date));
+    if (!isValid(date)) return res.json({ error: "Invalid Date" });
+    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+app.get("/api", function (req, res) {
+    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+});
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+    console.log("Your app is listening on port " + 3000);
+});
